b7631852dace87dbbcb53f1f3c01f33c7e80b161
Possums <brushtailpossums@gmail.com>
2017-02-10 19:44:01 +0000

    Add files via upload

--- DiepIOMap.java
+++ DiepIOMap.java
@@ -1,33 +1,45 @@
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import java.net.URL;
 
 public class DiepIOMap extends GameMap {
-	
-	Tank t = new Tank(Color.BLACK, 1 , 1, 10);
 
-	
-	public DiepIOMap(Dimension d) {
+	Tank t = new Tank(Color.BLACK, 1 , 1, 10);
+	Image background;
+
+	public DiepIOMap() {
 		addTank();
-		
-		
+
+
 	}
-	
+
 	public void draw(Graphics g){
-//		Tank t = new Tank(Color.BLACK, 1 , 1);
+		//		Tank t = new Tank(Color.BLACK, 1 , 1);
 		t.draw(g);
 	}
-	
-	
+
+
 	private void addTank() {
 		this.addGameObject(t);
-		
+
 	}
 
 
 	@Override
 	public void openBackgroundImage() {
 		// TODO Auto-generated method stub
+		try {
+			URL url = getClass().getResource("images/background.png");
+			background = ImageIO.read(url);
+		} catch (Exception e) {
+			System.out.println("problem opening the background");
+			e.printStackTrace();
+		}
 
 	}
 
