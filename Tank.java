3cab284d9bff46d3b896033a8d1fb7eccc68b68c
Possums <brushtailpossums@gmail.com>
2017-02-16 19:47:34 +0000

    Add files via upload

--- Tank.java
+++ Tank.java
@@ -17,7 +17,7 @@ public class Tank extends GameObject {
 	Dimension a = Toolkit.getDefaultToolkit().getScreenSize();
 	Point center = new Point(a.width/2, a.height/2);
 	public Tank(Color color, double x, double y, double size){
-		
+
 		super(color, x, y, size);
 		openImage();
 	}
@@ -30,42 +30,26 @@ public class Tank extends GameObject {
 
 	public void draw(Graphics g){
 		//g.drawRect((int)getX(), (int)getY(), (int)getSize(), (int)getSize());
-		
+		Point imagePosition = new Point((int)this.getX(), (int)this.getY());
 		Graphics2D g2d=(Graphics2D)g; // Create a Java2D version of g.
-		
-		//Point x = new Point((int)this.getX(), (int)this.getY());
-		
-		//System.out.println(mouseX());
-//		AffineTransform backup = g2d.getTransform();
-//		AffineTransform trans = new AffineTransform();
-//		trans.rotate(calculateAngle(), (int)this.getX(), (int)this.getY());
-//
-//		g2d.transform(trans);
-//		g2d.drawImage(tank, (int)this.getX(), (int)this.getY(), 2*(int)this.getSize(), (int)this.getSize(), null);
-//
-//
-//		g2d.setTransform(backup); 
-//		g2d.dispose();
+		System.out.println(tank.getHeight(null) + "height");
+		System.out.println(tank.getWidth(null)+ "");
+		int cx = tank.getWidth(null) / 2;
+		int cy = tank.getHeight(null) / 2;
+		AffineTransform oldAT = g2d.getTransform();
+		g2d.translate(cx+imagePosition.x, cy+imagePosition.y);
+		g2d.rotate(calculateAngle());
+		g2d.translate(-cx, -cy);
+		g2d.drawImage(tank, 0, 0, null);
+		g2d.setTransform(oldAT);
 
-		  	int cx = tank.getWidth(null) / 2;
-	        int cy = tank.getHeight(null) / 2;
-	        AffineTransform oldAT = g2d.getTransform();
-	        g2d.translate(cx+this.getX(), cy+this.getY());
-	        g2d.rotate(calculateAngle());
-	        g2d.translate(-cx, -cy);
-	        g2d.drawImage(tank, 0, 0, null);
-	        g2d.setTransform(oldAT);
-		
+
+
 	}
 
 	// 	public shoot(Color color, double x, double y, double size, double damage){
 	// 		Bullet b = new Bullet(color, x, y, size, damage);
 	// 	}
-	
-		public GameObject shoot() {
-		Bullet b = new Bullet(Color.BLACK, this.getX(), this.getY(), 2);
-		return b;
-	}
 
 	private int mouseX(){
 		PointerInfo a = MouseInfo.getPointerInfo();
@@ -82,7 +66,7 @@ public class Tank extends GameObject {
 	}
 
 	private double calculateAngle(){
-		
+
 		int adjacentSide = mouseX() - (int)this.getX();
 		int oppositeSide = mouseY() - (int)this.getY();
 		double angle = Math.atan2(oppositeSide,adjacentSide);
