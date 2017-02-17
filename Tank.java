bf85dae94880de3d1a35928673c17aabd940c9ef
Possums <brushtailpossums@gmail.com>
2017-02-17 18:38:02 +0000

    Add files via upload

--- Tank.java
+++ Tank.java
@@ -16,6 +16,7 @@ public class Tank extends GameObject {
 	Image tank;
 	Dimension a = Toolkit.getDefaultToolkit().getScreenSize();
 	Point center = new Point(a.width/2, a.height/2);
+	Point imageCenter = new Point((int)this.getX(), (int)this.getY());
 	public Tank(Color color, double x, double y, double size){
 
 		super(color, x, y, size);
@@ -31,13 +32,14 @@ public class Tank extends GameObject {
 	public void draw(Graphics g){
 		//g.drawRect((int)getX(), (int)getY(), (int)getSize(), (int)getSize());
 		Point imagePosition = new Point((int)this.getX(), (int)this.getY());
+		imageCenter = new Point((int)this.getX() + 89, (int)this.getY() -50);
 		Graphics2D g2d=(Graphics2D)g; // Create a Java2D version of g.
-		System.out.println(tank.getHeight(null) + "height");
-		System.out.println(tank.getWidth(null)+ "");
+		//System.out.println(tank.getHeight(null) + "height");
+		//System.out.println(tank.getWidth(null)+ "");
 		int cx = tank.getWidth(null) / 2;
 		int cy = tank.getHeight(null) / 2;
 		AffineTransform oldAT = g2d.getTransform();
-		g2d.translate(cx+imagePosition.x, cy+imagePosition.y);
+		g2d.translate(cx+imageCenter.x, cy+imageCenter.y);
 		g2d.rotate(calculateAngle());
 		g2d.translate(-cx, -cy);
 		g2d.drawImage(tank, 0, 0, null);
@@ -51,10 +53,11 @@ public class Tank extends GameObject {
 	// 		Bullet b = new Bullet(color, x, y, size, damage);
 	// 	}
 	
-			public GameObject shoot() {
+	public GameObject shoot() {
 		Bullet b = new Bullet(Color.BLACK, this.getX(), this.getY(), 2, this.getDirection());
 		return b;
 	}
+
 
 	private int mouseX(){
 		PointerInfo a = MouseInfo.getPointerInfo();
@@ -72,10 +75,11 @@ public class Tank extends GameObject {
 
 	private double calculateAngle(){
 
-		int adjacentSide = mouseX() - (int)this.getX();
-		int oppositeSide = mouseY() - (int)this.getY();
+		int adjacentSide = mouseX() - imageCenter.x;
+		int oppositeSide = mouseY() - imageCenter.y;
 		double angle = Math.atan2(oppositeSide,adjacentSide);
-		System.out.println(Math.toDegrees(angle));
+		System.out.println(this.getX() + ", " +this.getY());
+		//System.out.println("Height equals " + tank.getHeight(null) + "Width equals " + tank.getWidth(null));
 		return  angle;
 	}
 
